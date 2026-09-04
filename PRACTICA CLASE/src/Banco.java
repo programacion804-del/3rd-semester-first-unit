f159268cb78b4213c9814d4661fb4e874b727f71
ITM-10339 <ITM-10339@ADmatamoros.tecnm.mx>
2026-09-04 07:57:14 -0600

    new class named Banco

---
+++ PRACTICA CLASE/src/Banco.java
@@ -0,0 +1,12 @@
+
+/**
+ *
+ * @author ITM-10339
+ */
+public class Banco {
+  private int cuenta;
+  private int saldo;
+  private String nombreBanco;
+  private int maxRetiros=3;
+  private int  retirosRealizados=0;
+}
