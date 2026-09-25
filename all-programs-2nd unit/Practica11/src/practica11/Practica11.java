8f1f2c3def2e00db8fdccbfa3ff7ddda867e1284
ITM-10339 <ITM-10339@ADmatamoros.tecnm.mx>
2026-09-25 07:33:35 -0600

    Add practice 11

---
+++ all-programs-2nd unit/Practica11/src/practica11/Practica11.java
@@ -0,0 +1,24 @@
+
+package practica11;
+/**
+ *
+ * Hector Caleb Mosqueda Santes N° control 25260872
+ */
+public class Practica11 {
+   
+    public static void main(String[] args) {
+      int numero=5;
+      int MostrarNumeros;
+      if(numero<=0){
+         System.out.println("Programa terminado");
+     }else{
+          
+         for (int i=0;i<=5;i++){
+          MostrarNumeros=numero-i;
+          System.out.println(MostrarNumeros);
+         }
+         
+     }
+    }
+    
+}
